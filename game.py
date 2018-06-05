1b2e584bc7e1bdcd98c0897a6a0e9414875f71af
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-05 15:49:59 -0700

    Ask player if/she want to play again

--- game.py
+++ game.py
@@ -1,20 +1,27 @@
 from random import randint
 
-print("Hello! Guess a number between 1 and 100!")
-player_name = input("Enter your name: ")
-random_nums = randint(1, 100)
-count = []
-guess = 0
+while True:
+    print("Hello! Guess a number between 1 and 100!")
+    player_name = input("Enter your name: ")
+    random_nums = randint(1, 100)
+    count = 0
+    guess = 0
 
-while guess != random_nums:
-    guess = int(input("Enter a number between 1 and 100: "))
-    if guess not in range(1, 101):
-        print("Try again. Pick a number between 1 and 100: ")
+    while guess != random_nums:
+        guess = int(input("Enter a number between 1 and 100: "))
+        if guess not in range(1, 101):
+            print("Try again. Pick a number between 1 and 100: ")
 
-    elif guess > random_nums:
-        print("Too high.")
-        count.append(guess)
+        elif guess > random_nums:
+            print("Too high.")
+            count += 1
+        else:
+            print("Too low.")
+            count += 1
+    print("You guessed it in {} tries.".format(count))
+    play_again = input("Would you like to play again? Y/N: ")
+    if play_again.upper() == 'N':
+        print("Thanks for playing!")
+        break
     else:
-        print("Too low.")
-        count.append(guess)
-print("You guessed it in {} tries.".format(len(count)))
+        pass
