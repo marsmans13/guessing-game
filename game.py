50fffd58b9cbd9d4a1f81d6be9c0b1824c54d1ed
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-05 17:41:11 -0700

    User input range and scaled scores

--- game.py
+++ game.py
@@ -1,29 +1,34 @@
 from random import randint
 
-print("Hello! Guess a number between 1 and 100!")
+print("Hello!")
 player_name = input("Enter your name: ")
 
 lowest_score = None
 
 
 while True:
-    random_nums = randint(1, 100)
+    low = int(input("Enter the lower bound of your range: "))
+    high = int(input("Enter the lower bound of your range: "))
+    random_nums = randint(low, high)
     count = 0
     guess = 0
-    while guess != random_nums:
-        guess = int(input("Enter a number between 1 and 100: "))
+    while guess != random_nums and count < 6:
+        guess = int(input("Enter a number between {} and {}: ".format(low, high)))
         count += 1
-        if guess not in range(1, 101):
-            print("Try again. Pick a number between 1 and 100: ")
+        if guess not in range(low, high + 1):
+            print("Try again. Pick a number between {} and {}: ".format(low, high))
         elif guess > random_nums:
             print("Too high.")
         elif guess < random_nums:
             print("Too low.")
-    print("You guessed it in {} tries.".format(count))
-    # if count < lowest_score or lowest_score is None:
-    if lowest_score is None or count < lowest_score:
-        print("Your new low score is: {}".format(count))
-        lowest_score = count
+
+    if count == 10:
+        print("Too many tries.")
+    elif lowest_score is None or count < lowest_score:
+        lowest_score = round((count / (high - low)) * 100)
+        print("Your new low score is: {}".format(lowest_score))
+    elif random_nums == guess:
+        print("You guessed it in {} tries.".format(count))
     play_again = input("Would you like to play again? Y/N: ")
     if play_again.upper() == 'N':
         print("Thanks for playing!")
